f46152e2c1c9e3ff22fc977e75ff5896da66787c
Rizwan Raza <rizwan.raza987@gmail.com>
2021-10-09 00:14:14 +0530

    API keys updated

--- src/config/api.js
+++ src/config/api.js
@@ -3,8 +3,8 @@ export const API = 'https://tripcart.online';
 
 // export const CONSUMER_KEY = 'ck_9a40d82e47c124bc1be422682dd93eb25bdb5e3a';
 // export const CONSUMER_SECRET = 'cs_01bd2047d4ea33bdb4b6d8f6f034e0e072abdf4a';
-export const CONSUMER_KEY = 'ck_450a77938c4f79a3c75779c24875af7a77182eb4';
-export const CONSUMER_SECRET = 'cs_46167d1613dda8e8ace99aba672ea675d257097b';
+export const CONSUMER_KEY = 'ck_af2a899b1f94b6113d9749b2b0001fecb3a20785';
+export const CONSUMER_SECRET = 'cs_5b2027b89179b4a147b64b7987311cad755db24c';
 
 export default {
   API_ENDPOINT: API,
